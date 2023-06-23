32b2b47a26d95bd8b8196335964f97859aec8055
minchan <minchan@minchans-MBP-2.attlocal.net>
2023-06-23 15:20:49 -0500

    added doctests to triangular_number

--- triangular_number.py
+++ triangular_number.py
@@ -1,8 +1,17 @@
 def triangle_number(number):
-    """ Returns the triangular number of input.
-    .. math::
-        \sum_{i=1}^{\\infty} x_{i}
     """
+    Returns the sum of the number and all integers before it until 0.
+
+    @param number: number to find triangular number of
+    @return: integer of trianglular number
+    ---
+    >>> triangle_number(3)
+    6
+    >>> triangle_number(4)
+    10
+    """
+    # base case
     if number == 1:
         return 1
+    # recursive case
     return number + triangle_number(number - 1)
