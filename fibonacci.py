dc577e84d103b5b2c99413378710132b5a60d91c
minchan <minchan@minchans-MBP-2.attlocal.net>
2023-06-23 12:47:56 -0500

    added fibonacci

---
+++ fibonacci.py
@@ -0,0 +1,21 @@
+# iterative fibonacci
+"""
+def fibonacci(index):
+    a, b = 0, 1
+    for i in range(index):
+        a, b = b, a + b
+    return a
+"""
+
+# recursive fibonacci
+def fibonacci(index):
+    # base case
+    if index == 1 or index == 2:
+        return 1
+    
+    # recursive case
+    return fibonacci(index - 1) + fibonacci(index - 2)
+
+# test cases
+print(fibonacci(6)) # returns 8
+print(fibonacci(10)) # returns 55
